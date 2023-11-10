51705cb71a4b3e39118b95cae4c5061fe39472a9
vdshb <github.com/vdshb>
2023-11-11 00:06:20 +0300

    add wasm target with canary nodejs causes build time exception

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -14,12 +14,12 @@ kotlin {
         browser()
         nodejs()
     }
-//    @Suppress("OPT_IN_USAGE")
-//    wasmJs {
-//        binaries.library()
-//        browser()
-//        nodejs()
-//    }
+    @Suppress("OPT_IN_USAGE")
+    wasmJs {
+        binaries.library()
+        browser()
+        nodejs()
+    }
 
     @Suppress("UNUSED_VARIABLE")
     sourceSets {
@@ -28,12 +28,12 @@ kotlin {
     }
 }
 
-////See: https://github.com/Kotlin/kotlin-wasm-examples/blob/main/nodejs-example/build.gradle.kts
-//rootProject.the<NodeJsRootExtension>().apply {
-//    nodeVersion = "21.0.0-v8-canary202309143a48826a08"
-//    nodeDownloadBaseUrl = "https://nodejs.org/download/v8-canary"
-//}
-//
-//tasks.withType<KotlinNpmInstallTask>().configureEach {
-//    args.add("--ignore-engines")
-//}
+//See: https://github.com/Kotlin/kotlin-wasm-examples/blob/main/nodejs-example/build.gradle.kts
+rootProject.the<NodeJsRootExtension>().apply {
+    nodeVersion = "21.0.0-v8-canary202309143a48826a08"
+    nodeDownloadBaseUrl = "https://nodejs.org/download/v8-canary"
+}
+
+tasks.withType<KotlinNpmInstallTask>().configureEach {
+    args.add("--ignore-engines")
+}
